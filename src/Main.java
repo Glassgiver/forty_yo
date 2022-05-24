a6ebfc0c9c9edd834e042809768429ffd649e48e
Alfiya <alfiya.usmanova.2004@gmail.com>
2022-05-24 12:10:48 +0600

    clear variant, with final objects

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int C_year = 2022;
-        int C_month = 5;
-        int C_day = 23;
-
         Person me = new Person();
 
         me.howOldAmI();
